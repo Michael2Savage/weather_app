5806ce0031be73f822f2c7aced0519d4faf532b1
btwist540@yahoo.com <btwist540@yahoo.com>
2021-11-07 17:36:45 -0500

    Test commit from my laptop

--- main.js
+++ main.js
@@ -6,7 +6,6 @@ const city = document.getElementById("city_input");
 const button = document.getElementById('only_button');
 button.addEventListener('click', (e)=>{
     e.preventDefault();
-    // getAPIdata(city.value);
     createHTML(city.value);
 });
 
@@ -17,10 +16,31 @@ const getAPIdata = async (city) => {
     return result
 }
 
+// image animation selector function
+const select_animation = async (input) => {
+    let api_data = await getAPIdata(input);
+    if (api_data.data.weather[0].main == 'Clouds') {
+        return 'images/animated/cloudy-day-1.svg';
+    }
+    if (api_data.data.weather[0].main == 'Clear') {
+        return 'images/animated/day.svg';
+    }
+    if (api_data.data.weather[0].main == 'Rain') {
+        return 'images/animated/rainy-5.svg';
+    }
+    if (api_data.data.weather[0].main == 'Snow') {
+        return 'images/animated/snowy-2.svg';
+    }
+
+
+}
+
+
 // Create html element
 const createHTML = async (input) => {
     let api_data = await getAPIdata(input);
     document.getElementById('city_name').innerHTML = api_data.data.name;
-    document.getElementById('temp').innerHTML = `${((api_data.data.main.temp - 273.15)*9/5 + 32).toFixed(2)} &#8457;`;
+    document.getElementById("api_call_animation").setAttribute("src", await select_animation(input));
+    document.getElementById('temp').innerHTML = `${((api_data.data.main.temp - 273.15)*9/5 + 32).toFixed(0)} &#8457;`;
     document.getElementById('weather').innerHTML = api_data.data.weather[0].description
 }
